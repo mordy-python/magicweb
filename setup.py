9f22f7b7d6712d1e988e2fed7e6bc99e7ea49ed3
mordy <imky171@gmail.com>
2021-08-31 19:39:18 -0400

    changed version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as readme_file:
 
 setup_args = dict(
     name='Magicweb',
-    version='1.0.1',
+    version='1.0.2',
     description='A simple web framework',
     long_description_content_type="text/markdown",
     long_description=README,
